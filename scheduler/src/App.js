65c7e35def4f5877827477ca77b51629793f21c6
John Kang <JK@dhcp-10-105-141-239.wireless.northwestern.private>
2019-09-27 15:07:11 -0500

    Completed Slice 4

--- scheduler/src/App.js
+++ scheduler/src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "rbx/index.css";
+import { Button, Container, Title } from "rbx";
 
 const schedule = {
   title: "CS Courses for 2018-2019",
@@ -26,7 +28,7 @@ const schedule = {
   ]
 };
 
-const Banner = ({ title }) => <h1>{title}</h1>;
+const Banner = ({ title }) => <Title>{title}</Title>;
 
 const terms = { F: "Fall", W: "Winter", S: "Spring" };
 
@@ -35,24 +37,24 @@ const getCourseTerm = course => terms[course.id.charAt(0)];
 const getCourseNumber = course => course.id.slice(1, 4);
 
 const Course = ({ course }) => (
-  <button>
+  <Button>
     {getCourseTerm(course)} CS {getCourseNumber(course)}: {course.title}
-  </button>
+  </Button>
 );
 
 const CourseList = ({ courses }) => (
-  <div>
+  <Button.Group>
     {courses.map(course => (
       <Course key={course.id} course={course} />
     ))}
-  </div>
+  </Button.Group>
 );
 
 const App = () => (
-  <div>
+  <Container>
     <Banner title={schedule.title} />
     <CourseList courses={schedule.courses} />
-  </div>
+  </Container>
 );
 
 export default App;
